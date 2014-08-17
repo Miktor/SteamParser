af946901cec2c10578c96b4161867a667052ca2d
Gladky Dmitry <miktor.ru@gmail.com>
2014-08-18 01:19:01 +0400

    remove pragma once

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,12 +1,8 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+#ifndef stdafx_h__
+#define stdafx_h__
 
-#pragma once
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
 
-
-// TODO: reference additional headers your program requires here
+#endif // stdafx_h__
